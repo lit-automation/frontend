f48e5e40c11dc7a7d8e22b19a022d8645b82727d
Wim Spaargaren <wim@weave.nl>
2020-06-25 10:54:14 +0200

    Screening explanation text corrections

--- src/components/lit-pages/ScreeningExplanation.ts
+++ src/components/lit-pages/ScreeningExplanation.ts
@@ -41,13 +41,13 @@ export class ScreeningExplanation extends LitElement {
         </div>
         <div class="row">
         <p>
-        Before starting to screening at the <a href="/screen-abstract">abstract screening page</a> you'll need to initialize the model
+        Before starting to screen at the <a href="/screen-abstract">abstract screening page</a> you'll need to initialize the model
         with atleast one included and one excluded article at the <a href="/articles">article overview page</a>. Based
         on verification done on screening data sets,
         we recommend to initialize the model with some additional included articles to reduce recall. Note that this also results
         in more incorrect results when starting to screen.
-        When the model is initialized, it is recommended to screen 30% of your article set using
-        the <a href="/screen-abstract">abstract screening page</a> before letting the model automatically screen the remaining articles.
+        When the model is initialized, it is recommended to screen 30% of the articles in your current set using
+        the <a href="/screen-abstract">abstract screening page</a>, before letting the model automatically screen the remaining articles.
         <br/>
         <br/>
         Verification data of screening abstracts is shown in the image below.
@@ -64,8 +64,8 @@ export class ScreeningExplanation extends LitElement {
         <div class="row">
         <p>
         As well as for screening on abstract, you'll need to initialize the model
-        with atleast one included and one excluded article at the <a href="/articles">article overview page</a>
-        Afterwards it is recommended to screen 50% of the articles using
+        with atleast one included and one excluded article at the <a href="/articles">article overview page</a>.
+        Afterwards, it is recommended to screen 50% of the articles using
         the <a href="/screen-full-text">full text screening page</a> before letting the model automatically screen the remaining articles.
         <br/>
         <br/>
